04928de17d707a35876c53266fcce60ded7aba79
TypeError441 <78857657+TypeError441@users.noreply.github.com>
2026-02-02 07:58:08 -0800

    Version 6.4.3
    Add styling back to feebdack

--- src/components/overlay/dialog/FeedbackDialog.vue
+++ src/components/overlay/dialog/FeedbackDialog.vue
@@ -1,13 +1,18 @@
 <script setup>
 import { ref } from "vue";
 
-import { useId } from "../../composables/settings";
+import { useId } from "../../../composables/settings";
 import pkg from "../../../../package.json";
+
+import CloseDialog from "../CloseDialog.vue";
+import { useCurrentDialog } from "../../../composables/settings";
 
 const id = useId();
 const version = ref(pkg.version);
 
 const emit = defineEmits(["close"]);
+
+const currentDialog = useCurrentDialog();
 
 const feedbackText = ref("");
 
@@ -23,42 +28,33 @@ function submit() {
         });
     }
 
-    emit("close");
-}
-
-function exit() {
-    emit("close");
+    currentDialog.value = "none";
 }
 </script>
 
 <template>
-<form name="feedback" id="feedback-modal" netlify>
-    <div id="feedback--title">Feedback</div>
-    <div id="feedback--subtitle">If you have questions, tell me your name or ask irl (I can't answer through the website)</div>
-    <textarea name="feedback-modal" id="feedback--textarea" maxlength="200" v-model.trim="feedbackText"></textarea>
-    <div id="feedback--choice-area">
-        <button type="submit" class="button" @click="submit">Submit</button>
-        <button type="button" class="button button__danger" @click="exit">Exit</button>
-    </div>
-</form>
+<dialog id="overlay--dialog__feedback" class="overlay--dialog glass" :open="currentDialog == 'feedback'">
+    <form name="feedback" id="feedback-modal" @submit.prevent="submit" netlify>
+        <CloseDialog />
+        <div id="feedback--title">Feedback</div>
+        <div id="feedback--subtitle">If you have questions, tell me your name or ask irl (I can't answer through the website)</div>
+        <textarea name="feedback-modal" id="feedback--textarea" class="glass glass__child" maxlength="200" v-model.trim="feedbackText"></textarea>
+        <button type="submit" class="button glass glass__child">Submit</button>
+    </form>
+</dialog>
 </template>
 
 <style scoped type="scss">
 #feedback-modal {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    position: absolute;
-    top: 50vh;
-    left: 50vw;
-    width: 50vw;
-    height: 50vh;
-    transform: translate(-50%, -50%);
-    background-color: rgb(var(--background-color));
+    width: 100%;
+    height: 100%;
     padding: 1em;
     border-radius: 0.5em;
     text-align: left;
-    pointer-events: all;
+    padding: 2rem;
+    color: rgb(var(--color));
 
     * {
         flex: 1;
@@ -76,25 +72,18 @@ function exit() {
     }
 
     #feedback--textarea {
+        --glass-color: var(--radial-background-color) !important;
         width: 100%;
-        background-color: rgb(var(--radial-background-color));
-        color: rgb(var(--color));
-        border: none;
+        padding: 2em;
         resize: none;
         margin: 1em 0;
+        color: rgb(var(--color));
     }
 
-    #feedback--choice-area {
-        flex-grow: 0;
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-
-        .button {
-            flex: 1;
-            margin: 0 0.25em;
-            width: 100%;
-        }
+    .button {
+        flex: 0;
+        margin: 0 0.25em;
+        width: 10rem;
     }
 }
 </style>
